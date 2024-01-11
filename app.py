531bd59008aa93530a471f00c44930a73e839cc5
John Liu <johnliu@v1041-wn-rt-b-173-254.campus-dynamic.uwaterloo.ca>
2024-01-11 01:28:36 -0500

    static path files

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from predict import predict_root
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['ENV'] = 'production'
