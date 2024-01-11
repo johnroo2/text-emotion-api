e4700d771877a601b938bc805ba497bf8f89faa8
John Liu <johnliu@v1041-wn-rt-b-173-254.campus-dynamic.uwaterloo.ca>
2024-01-11 01:40:39 -0500

    file hosting

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS, cross_origin
 from predict import predict_root
 
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['ENV'] = 'production'
@@ -17,6 +17,11 @@ def index():
     label, probs = prediction[0], prediction[1]
     probs = [float(element) for element in probs]
     return jsonify({"label": label, "probs": probs})
+
+@app.route('/.well-known/pki-validation/39D3AE96C00146317DED8ECBBA47F9AA.txt')
+def serve_txt_file():
+    file_path = './.well-known/pki-validation/39D3AE96C00146317DED8ECBBA47F9AA.txt'
+    return send_file(file_path, mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run()
